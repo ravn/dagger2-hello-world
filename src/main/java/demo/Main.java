d8f86dad67c0b22e79455496dcc200ac64dd3e46
tra <tra@kb.dk>
2017-09-20 15:23:03 +0200

    Reorganized Main.java to present in the same order as README.md

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,33 +1,29 @@
 package demo;
 
-import dagger.Component;
-import dagger.Module;
-import dagger.Provides;
-
-interface HelloWorld {
+interface HelloWorld { // Our work unit.
     String getMessage();
 }
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-    	// If compilation fails, see README.md
-        HelloWorldComponent daggerGeneratedComponent = DaggerMain_HelloWorldComponent.builder().build();
-		
-        HelloWorld helloWorld = daggerGeneratedComponent.helloWorld();
-        System.out.println(helloWorld.getMessage());
-    }
-
-    // What do we need?
-    @Component(modules = HelloWorldModule.class)
-    interface HelloWorldComponent {
-        HelloWorld helloWorld();
+@dagger.Module // What help does Dagger need?
+class HelloWorldModule {
+    @dagger.Provides
+    HelloWorld provideHelloWorld() {
+        return () -> "Hello World";
     }
 }
 
-@Module // How do we make what is needed? (Except classes with @Inject constructor)
-class HelloWorldModule {
-    @Provides
-    HelloWorld provideHelloWorld() {
-        return () -> "Hello World";
+public class Main {
+    // What do we need Dagger to build?
+    @dagger.Component(modules = HelloWorldModule.class)
+    interface HelloWorldComponent {
+        HelloWorld helloWorld();
+    }
+
+    public static void main(String[] args) throws Exception {
+        // If compilation fails, see README.md
+        HelloWorldComponent daggerGeneratedComponent = DaggerMain_HelloWorldComponent.builder().build();
+
+        HelloWorld helloWorld = daggerGeneratedComponent.helloWorld();
+        System.out.println(helloWorld.getMessage());
     }
 }
